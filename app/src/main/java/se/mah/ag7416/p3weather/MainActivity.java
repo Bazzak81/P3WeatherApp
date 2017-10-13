cf9a7a131e120332034922b6442119796d489959
Bazzak <bazzak81@gmail.com>
2017-10-13 11:36:37 +0200

    Test

--- app/src/main/java/se/mah/ag7416/p3weather/MainActivity.java
+++ app/src/main/java/se/mah/ag7416/p3weather/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Testar lite till va fan
+        //Testar lite till va fan!
 
     }
 }
