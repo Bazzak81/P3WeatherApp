8eee38111d101021c68dc08b393e7a30e3cbf7e0
Bazzak <bazzak81@gmail.com>
2017-10-23 14:42:13 +0200

    körde gradel builder

--- app/src/main/java/se/mah/ag7416/p3weather/Activities/Controllers/Connection.java
+++ app/src/main/java/se/mah/ag7416/p3weather/Activities/Controllers/Connection.java
@@ -1,14 +1,11 @@
 package se.mah.ag7416.p3weather.Activities.Controllers;
 
-import android.util.Log;
-
-import org.json.JSONObject;
-
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
-import static android.content.ContentValues.TAG;
 
 /**
  * Created by Jimmy Åkesson on 2017-10-23.
@@ -17,12 +14,39 @@ import static android.content.ContentValues.TAG;
 public class Connection {
 
     //API key: 92d45b077fa249614bfc79c61cf8b50f
-    private String apiKey = "92d45b077fa249614bfc79c61cf8b50f";
-    private String httpPath = "api.openweathermap.org/data/2.5/weather?q=";
+    private String apiKey = "&APPID=92d45b077fa249614bfc79c61cf8b50f";
+    private String httpPath = "http://api.openweathermap.org/data/2.5/weather?q=";
 
 
     public Connection() {
 
+    }
 
+    public String getWeather(String city) {
+        HttpURLConnection connection;
+        InputStream inputStream;
+
+        try {
+            connection = (HttpURLConnection) (new URL(httpPath + city + apiKey)).openConnection();
+            connection.setRequestMethod("GET");
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            connection.connect();
+
+            StringBuilder buffer = new StringBuilder();
+            inputStream = connection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                buffer.append(line).append("\r\n");
+            }
+            inputStream.close();
+            connection.disconnect();
+            return buffer.toString();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
