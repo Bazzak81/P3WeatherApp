363038b400cf54a44f0cf4ce3a2e04af0d6f69ac
jullemeister <olsson_olle@telia.com>
2017-10-24 13:00:23 +0200

    activityshit

--- app/src/main/java/se/mah/ag7416/p3weather/Activities/Activities/FragmentActivity.java
+++ app/src/main/java/se/mah/ag7416/p3weather/Activities/Activities/FragmentActivity.java
@@ -1,8 +1,13 @@
 package se.mah.ag7416.p3weather.Activities.Activities;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+
+import se.mah.ag7416.p3weather.Activities.Fragments.WeatherFragment;
+import se.mah.ag7416.p3weather.R;
 
 /**
  * Created by OlleOlsson on 2017-10-18.
@@ -10,16 +15,27 @@ import android.view.Menu;
 
 public class FragmentActivity extends AppCompatActivity {
 
+    private WeatherFragment weatherFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        weatherFragment = new WeatherFragment();
+        addFragment(weatherFragment);
 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         return true;
+    }
+
+    public void addFragment(Fragment fragment) {
+        FragmentTransaction swap = getSupportFragmentManager().beginTransaction();
+        swap.replace(R.id.fragment_container, fragment);
+        swap.commit();
     }
 }
